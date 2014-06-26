203fece9ceb16e510bdf5d9fd670cf5d435ac7ab
shahrilhafiz.zainol@gmail.com <shahrilhafiz.bin-zai@VIE-SHAHRIL-NB3.vitrox.local>
2014-06-26 16:12:47 +0800

    Tested DroidCore Algorithm with additional speed control for each servo.

--- Software/DroidCore/DroidCore.ino
+++ Software/DroidCore/DroidCore.ino
@@ -1,22 +1,33 @@
 #include <Servo.h>
 
-struct Actuator 
+#define INIT_POSE 0
+
+struct Actuator//Servo Object Wrapper Declaration 
 {
   Servo  ServoObj;
   byte   CurrentPosition;    
+  byte   Speed;
 };
 
 Actuator ServoActuator[10];//delcare 10 Servo Motor Object
 
+//global variable
 byte CurrentStep;
-byte FramePosition[10];
-int  StepSize,Increment,i;
-byte FrameFinish = 10;
+byte FramePosition[10] = {0,0,0,0,0,0,0,0,0,0};//buffer to store action frame position
+int  StepSize,i;
+unsigned short FrameFinish, Flag;//16-bits flag to check servo status
+
+int servo_en = 13;
 
 unsigned long ServoTimer;
 
 void setup()
 {
+  StepSize = 1;
+ 
+  pinMode(servo_en, OUTPUT); 
+  digitalWrite(servo_en, LOW);   
+  
   ServoActuator[0].ServoObj.attach(1);
   ServoActuator[1].ServoObj.attach(2);
   ServoActuator[2].ServoObj.attach(3);
@@ -27,57 +38,87 @@ void setup()
   ServoActuator[7].ServoObj.attach(8);
   ServoActuator[8].ServoObj.attach(9);
   ServoActuator[9].ServoObj.attach(10);
+  
+  ServoActuator[2].CurrentPosition = 45;
+  ServoActuator[2].ServoObj.write(ServoActuator[2].CurrentPosition); 
+  ServoActuator[3].Speed = 5;
+  ServoActuator[2].Speed = 1;
+  //action(INIT_POSE);//Make droid initial pose
+  FrameFinish = 0x000;
+  ServoTimer = millis();
 }
 
 void loop()
 {
-  
+  action(1);  
 }
 
-void action()
+void action(byte action_number)
 {  
-  int FrameStatus;
+  int MotorStatus;
+  
   if((millis() - ServoTimer) >= 20)
-  {
-    FrameFinish = 10;
-    for(i=0; i++; i<10)
+  {    
+    
+    for(i=0; i<10; i++)
     {
-      FrameStatus = Move(i,FramePosition[i]);
-      if(FrameStatus == 0)
-        FrameFinish--;  
-    }    
+      digitalWrite(servo_en, HIGH);
+      MotorStatus = Move(i,FramePosition[i]);
+      if((MotorStatus == 0))
+      {
+        FrameFinish = (FrameFinish | (0x1<<i));
+      }  
+    } 
     ServoTimer = millis();
-  }
+    if(FrameFinish == 0x3FF)
+    {
+      FrameFinish = 0x000;
+      UpdatePosition();
+    }
+  }  
 }
 
 void UpdatePosition()
 {
   
+  static boolean p = true;
+  if(p)
+  {
+    FramePosition[2] = 45;
+    FramePosition[3] = 5;
+    p = false;
+  }
+  else
+  {
+    FramePosition[2] = 135;
+    FramePosition[3] = 180;
+    p = true;
+  }
 }
 
 int Move(byte channel, byte target)
 {
  int newIcrement = 0;   
- newIcrement = IncrementCal(ServoActuator[channel].CurrentPosition, target);
+ newIcrement = IncrementCal(ServoActuator[channel].CurrentPosition, target, ServoActuator[channel].Speed);
  ServoActuator[channel].CurrentPosition += newIcrement;
- ServoActuator[0].ServoObj.write(ServoActuator[channel].CurrentPosition);  
+ ServoActuator[channel].ServoObj.write(ServoActuator[channel].CurrentPosition);  
  return newIcrement;
 }
 
-int IncrementCal(byte CurrentPos, byte TargetPos)
+int IncrementCal(byte CurrentPos, byte TargetPos, byte Speed)
 {
  static int check = 0; 
  static int IncResult = 0; 
  check = CurrentPos-TargetPos;
- if((check>=(-StepSize))&check<0)
+ if((check>=(-Speed))&check<0)
    IncResult = -check;
- else if((check<StepSize)&check<0)
-   IncResult = StepSize;
+ else if((check<Speed)&check<0)
+   IncResult = Speed;
  
- if((check<=StepSize)&check>0)
+ if((check<=Speed)&check>0)
    IncResult = -check;
- else if((check>StepSize)&check>0)
-   IncResult = -StepSize;
+ else if((check>Speed)&check>0)
+   IncResult = -Speed;
    
  if(check==0)
    IncResult = 0;
